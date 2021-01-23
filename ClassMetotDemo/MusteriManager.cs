0965e631cdd4ee6b01f847f0eb13bf880cb12787
sabri uzunay <sabriuzunay2016@gmail.com>
2021-01-24 00:03:04 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri ekleme)
+        {
+            Console.WriteLine(ekleme.adi +" "+ ekleme.soyadi + " Bankamıza Hoş geldiniz ");
+        }
+        public void Silme(Musteri silme)
+        {
+            Console.WriteLine(silme.adi + " Bankamızı kulandığınız için teşekür ederiz");
+        }
+        public void listeleme(Musteri liste)
+        {
+            Console.WriteLine(liste.id + ". sıralı müşterimiz");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.id = 1;
+            musteri1.adi = "Sabri";
+            musteri1.soyadi = "uzunay";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.id = 2;
+            musteri2.adi = "Tunahan";
+            musteri2.soyadi = "Bayrakçeken";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Silme(musteri1);
+            musteriManager.listeleme(musteri2);
+        }
+    }
+}
